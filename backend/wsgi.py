c3fe4d1025640b385cd3f6f24c43e7c28bf3738e
alexander2309 <alexanderdelrosarioperez@gmail.com>
2026-06-04 08:58:03 -0400

    Restore clean wsgi.py

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -1,18 +1,7 @@
 import os
-import traceback
-import sys
+
+from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 
-_wsgi_error = None
-try:
-    from django.core.wsgi import get_wsgi_application
-    application = get_wsgi_application()
-except Exception:
-    _wsgi_error = traceback.format_exc()
-    print(_wsgi_error, file=sys.stderr, flush=True)
-
-if _wsgi_error:
-    def application(environ, start_response):
-        start_response('500 Internal Server Error', [('Content-Type', 'text/plain')])
-        return [f'Django startup failed\n{_wsgi_error}'.encode()]
+application = get_wsgi_application()
